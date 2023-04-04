506b136232af104b9765355dcb26ff8b80fbf1ac
rayz1065 <rayz1065@gmail.com>
2023-04-04 21:58:50 +0200

    The solution to easy1 was outdated...

--- olinfo/easy1.cpp
+++ olinfo/easy1.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 int main(){
-    freopen("input.txt", "r", stdin); //usa questa riga di codice per dire "l'input prendilo dal file input.txt (nella stessa directory del file .cpp)"
-    freopen("output.txt", "w", stdout); // usa questa riga di codice per dire "l'output scrivimelo nel file output.txt (sempre nella stessa directory"
-    int mas=0 ,user=0, N=0;
-    cin>>N;
-    for(int cont=0; cont<N; cont++){
-        cin>>user;
-        if(user>mas){
-            mas=user;
+    freopen("input.txt", "r", stdin);       //usa questa riga di codice per dire "l'input prendilo dal file input.txt (nella stessa directory del file .cpp)"
+    freopen("output.txt", "w", stdout);     // usa questa riga di codice per dire "l'output scrivimelo nel file output.txt (sempre nella stessa directory"
+    int mas=0,user=0, N=0;
+    cin>>N;                                 //n rappresenta il numero di input
+    cin>>mas;
+    for(int cont=1; cont<N; cont++){        //sarà necessario partire dal primo numero di input e arrivare al ennesimo numero
+        cin>>user;                          //i dati vengono inseriti nella variabile temporanea user
+        if(user>mas){                       //l'ultimo dato estratto viene confrontato con il valore maggiore al momento
+            mas=user;                       //se è maggiore allora questo sarà il dato che verrà confrontato
         }
     }
-    cout<<mas;
+    cout<<mas;                              //viene mandato un output
 }
