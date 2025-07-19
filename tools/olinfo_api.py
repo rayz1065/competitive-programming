bc727a5889ea7358b2fbeb0f2b4d53368050b30b
rayz1065 <rayz1065@gmail.com>
2025-07-19 12:58:51 +0200

    Update olinfo api tool to support recent updates

--- tools/olinfo_api.py
+++ tools/olinfo_api.py
@@ -1,10 +1,16 @@
+import argparse
+import itertools
+import json
+import os
 import re
 import sys
-import os
+import time
+from urllib.parse import quote_plus
+
 import requests
-import argparse
 
 TRAINING_TOKEN_ENVVAR = "TRAINING_TOKEN"
+TRAINING_BETTER_AUTH_SESSION_TOKEN_ENVVAR = "TRAINING_BETTER_AUTH_SESSION_TOKEN"
 
 
 def parse_args(argv):
@@ -21,6 +27,7 @@ def parse_args(argv):
             "terry",
             "ois",
             "itoi",
+            "naoi",
         ],
         help="The target competition",
     )
@@ -28,6 +35,11 @@ def parse_args(argv):
         "--token",
         help="The token to use for the API requests",
         default=os.environ.get(TRAINING_TOKEN_ENVVAR),
+    )
+    parser.add_argument(
+        "--better_auth_session_token",
+        help="The token to use for the API requests",
+        default=os.environ.get(TRAINING_BETTER_AUTH_SESSION_TOKEN_ENVVAR),
     )
     parser.add_argument(
         "--readme",
@@ -43,25 +55,35 @@ def parse_args(argv):
 
 
 def get_search(target):
+    res = None
     if target == "weoi":
-        return {"search": "weoi_"}
+        res = {"search": "weoi_"}
+    if target == "naoi":
+        res = {"search": "naoi_"}
     if target == "itday":
-        return {"search": "itday_"}
+        res = {"search": "itday_"}
     if target == "abc":
-        return {"tag": "abc"}
+        res = {"tag": "abc"}
     if target == "gator":
-        return {"tag": "gator"}
+        res = {"tag": "gator"}
     if target == "oii":
-        return {"tag": "nazionali"}
+        res = {"tag": "nazionali"}
     if target == "pre-oii":
-        return {"tag": "pre-oii"}
+        res = {"tag": "pre-oii"}
     if target == "terry":
-        return {"tag": "territoriali"}
+        res = {"tag": "territoriali"}
     if target == "ois":
-        return {"tag": "ois"}
+        res = {"tag": "ois"}
     if target == "itoi":
-        return {"tag": "ioit"}
-    raise SystemExit(f"Invalid target {target}")
+        res = {"tag": "ioit"}
+
+    if res is None:
+        raise SystemExit(f"Invalid target {target}")
+
+    res.setdefault("tag", "")
+    res.setdefault("search", "")
+
+    return res
 
 
 def get_url(task):
@@ -114,29 +136,71 @@ def update_readme(args, tasks):
     f.writelines(readme_contents)
 
 
+def get_tasks(args):
+    search_data = get_search(args.target)
+    search = quote_plus(search_data["search"])
+    tag = quote_plus(search_data["tag"])
+
+    tasks = []
+    for page in itertools.count():
+        print(f"Fetching page {page + 1}...", file=sys.stderr)
+
+        response = requests.get(
+            f"https://training.olinfo.it/tasks/{page + 1}?tag={tag}&search={search}",
+            headers={
+                "User-Agent": "Task syncer (https://github.com/rayz1065/competitive-programming)",
+                "Accept": "text/x-component",
+                # magic next stuff
+                "RSC": "1",
+                "Next-Router-State-Tree": quote_plus(
+                    """["",{"children":["(training)",{"children":["tasks",{"children":[["page","2","d"],{"children":["__PAGE__",{},null,null]},null,null]},null,null]},null,null]},null,null,true]"""
+                ),
+            },
+            cookies={
+                "training_token": args.token,
+                "better-auth.session_token": args.better_auth_session_token,
+            },
+            timeout=args.api_timeout,
+        )
+        response.encoding = response.apparent_encoding
+
+        print(
+            f"Fetched page {page + 1} with status {response.status_code}",
+            file=sys.stderr,
+        )
+
+        if not response.ok:
+            raise SystemExit(f"Failed to fetch tasks: {response.text}")
+
+        response_arr = response.text.strip().split("\n")
+        found = [x for x in response_arr if '"taskList":' in x]
+        if not found and page > 0:
+            break
+
+        if len(found) > 1:
+            print("\n\n".join(found), file=sys.stderr)
+            raise SystemExit("Tasks list duplicates in response")
+        if not found:
+            print("\n\n".join(response_arr), file=sys.stderr)
+            raise SystemExit("Tasks list not found in response")
+
+        data = json.loads(found[0].split(":", maxsplit=1)[1])
+
+        data = [x for x in data if isinstance(x, dict) and "taskList" in x]
+        tasks_page = data[0]["taskList"]
+
+        tasks.extend(tasks_page)
+        time.sleep(0.1)
+
+    return tasks
+
+
 def main(argv):
     args = parse_args(argv)
     if not args.token:
         raise SystemExit(f"Set {TRAINING_TOKEN_ENVVAR} or pass --token")
 
-    response = requests.post(
-        "https://training.olinfo.it/api/task",
-        headers={
-            "Content-Type": "application/json",
-            "Cookie": f"training_token={args.token}",
-        },
-        json={
-            "action": "list",
-            "first": 0,
-            "last": 9999,
-            **get_search(args.target),
-        },
-        timeout=args.api_timeout,
-    )
-    if not response.ok:
-        raise SystemExit(f"Failed to fetch tasks: {response.text}")
-
-    tasks = response.json()["tasks"]
+    tasks = get_tasks(args)
     print(f"Found {len(tasks)} tasks", file=sys.stderr)
 
     if args.readme:
